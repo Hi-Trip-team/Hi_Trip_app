96de16bed47a966975e6daef0a93f0997e028de0
ETi0n <ysl04030@gmail.com>
2026-03-02 17:12:51 +0900

    chore: initial project

---
+++ HiTrip/HiTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiTripApp.swift
+//  HiTrip
+//
+//  Created by ETiOn on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct HiTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
